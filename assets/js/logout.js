73d7d4122f2c35f94fb2eff82782c1ce6706fa11
Mimenee <imene.toudeft@hotmail.com>
2023-06-08 10:46:19 +0200

    logout feature with its js file in assets and token_function in inc folder

---
+++ assets/js/logout.js
@@ -0,0 +1,9 @@
+let seconds = 5;
+const countdown = document.getElementById('countdown');
+
+setInterval(() => {
+    if (seconds > 0) {
+        seconds--;
+    }
+    countdown.innerText = seconds;
+}, 1000)
