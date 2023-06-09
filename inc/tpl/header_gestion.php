1246df69dc3a4bfc812f070452490db7fe855f47
lucassrblt <lrimbault92@gmail.com>
2023-06-09 15:43:59 +0200

    adding management nav

---
+++ inc/tpl/header_gestion.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class='logo'>
+        <div class='logo-txt'>
+            <p>OIKOS <span>GESTION</span></p>
+            <div class="underline"></div>
+        </div>
+    </div>
+    <div class='icon'>
+        <div class="icon-heart"><img src=<?= $heart_icon ?> alt=""></div>
+        <div class="icon-account-menu">
+            <div class="icon-menu"><img src=<?= $menu_icon ?> alt=""></div>
+            <div class="icon-account"><img src=<?= $account_icon ?> alt=""></div>
+        </div>
+    </div>
+</nav>
