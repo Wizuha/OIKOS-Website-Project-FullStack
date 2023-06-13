f02c5b6c04f0c63144b3f0202b14ebe355eca3d5
william <williams_wandji@yahoo.fr>
2023-06-13 10:15:19 +0200

    ret

--- client_zone/booking/booking_details.php
+++ client_zone/booking/booking_details.php
@@ -8,7 +8,7 @@ if(isset($_GET['booking_id'])){
 
 $get_booking_details = $website_pdo -> prepare('
     SELECT DISTINCT b.housing_id, b.price, b.start_date_time, b.end_date_time, b.booking_date_time,
-    h.title, h.place,h.district, h.number_of_pieces, h.area, h.price, h.description, h.capacity, h.type
+    h.title, h.place,h.district, h.number_of_pieces, h.area, h.price, h.description, h.capacity, h.type, DATEDIFF(b.end_date_time,b.start_date_time)
     FROM booking b
     JOIN housing h ON h.id = b.housing_id
     WHERE b.user_id = :user_id AND b.id = :booking_ID
@@ -34,6 +34,7 @@ if($booking_details){
     $start_date_time = $booking_details['start_date_time'];
     $end_date_time = $booking_details['end_date_time'];
     $booking_date_time = $booking_details['booking_date_time'];
+    $nb_day_booking = $booking_details['DATEDIFF(b.end_date_time,b.start_date_time)'];
 }
 
 ?>
@@ -59,6 +60,7 @@ if($booking_details){
         <li><B>Type:</B><?php echo $type ?></li>
         <li><B>Début du séjour:</B><?php echo $start_date_time ?></li>
         <li><B>Fin du séjour:</B><?php echo $end_date_time ?></li>
+        <li><B>Nombre de jour:</B><?php echo $nb_day_booking  ?> jours</li>
         <li><B>Réservation fait le:</B><?php echo $booking_date_time ?></li>
     </ul>
     <a href="./booking_history.php"><button>Retour</button></a>
