07c3daeb6d779485407a85b6d553346750c60865
GDKobrae <djedjegboble.pro@gmail.com>
2023-06-14 16:43:22 +0200

    Starting Manangement housing.php

--- management_zone/housing.php
+++ management_zone/housing.php
@@ -1,12 +1,31 @@
-<?php 
+<?php
+    
+    require '../inc/pdo.php';
+    session_start();
+    if (!isset($_GET['housing_id'])) {
+        header('Location: ./housing_list.php');
+    } else {
+        $housing_id = $_GET['housing_id'];
+        $housing_info_request = $website_pdo->prepare('
+            SELECT id FROM housing
+            WHERE id = :id
+        ');
+        $housing_info_request->execute([
+            ':id' => $housing_id
+        ]);
+        $housing_info_request_result = $housing_info_request->fetch(PDO::FETCH_ASSOC);
+
+        if (!$housing_info_request_result) {
+            header('Location: ./housing_list.php');
+        }
+    }
+
     $heart_icon = '../assets/images/heart.svg';
     $menu_icon =   '../assets/images/menu.svg';
     $account_icon = '../assets/images/account.svg';
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
+?><!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,7 +33,7 @@
     <link rel="stylesheet" href="../assets/css/font.css">
     <link rel="stylesheet" href="../assets/css/header_gestion.css">
     <link rel="stylesheet" href="../assets/css/global.css">
-    <title>Document</title>
+    <title>Logement</title>
 </head>
 <body>
     <?php require '../inc/tpl/header_gestion.php' ?>
