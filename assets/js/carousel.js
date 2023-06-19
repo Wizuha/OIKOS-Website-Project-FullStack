c000d27c7d7c6781848c1b79dcd5a06daf93721e
lucassrblt <lrimbault92@gmail.com>
2023-06-19 10:35:36 +0200

    Adding header and favorites

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,10 +1,26 @@
-function previous() {
-    console.log('ok')
-    const widthSlider = document.querySelector('.house-img').offsetWidth //Récupère la largeur actuelle du slider
-    document.querySelector('.slider-content').scrollLeft -= widthSlider
-}
+const images = document.getElementsByClassName('house-img')
+const slidersContent = document.getElementsByClassName('slider-content')
 
-function next() { // Objectif décaler le scroll
-    const widthSlider = document.querySelector('.house-img').offsetWidth //Récupère la largeur actuelle du slider
-    document.querySelector('.slider-content').scrollLeft += widthSlider
-}
+    Array.from(slidersContent).forEach(sliderContent => {
+        function previous() {
+            console.log('ok')
+            const widthSlider = document.querySelector('.house-img').offsetWidth //Récupère la largeur actuelle du slider
+            sliderContent.scrollLeft -= widthSlider
+        }
+        function next() { // Objectif décaler le scroll
+            const widthSlider = document.querySelector('.house-img').offsetWidth //Récupère la largeur actuelle du slider
+            sliderContent.scrollLeft += widthSlider
+        }
+    })
+
+
+// function previous() {
+//     console.log('ok')
+//     const widthSlider = document.querySelector('.house-img').offsetWidth //Récupère la largeur actuelle du slider
+//     document.querySelector('.slider-content').scrollLeft -= widthSlider
+// }
+
+// function next() { // Objectif décaler le scroll
+//     const widthSlider = document.querySelector('.house-img').offsetWidth //Récupère la largeur actuelle du slider
+//     document.querySelector('.slider-content').scrollLeft += widthSlider
+// }
