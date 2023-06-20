a96c7e5f159a3612d1475c6929d79fd183154358
Clement Asensio <clemasensio@gmail.com>
2023-06-20 11:34:43 +0200

    homepage

---
+++ public_zone/fiche_appart.php
@@ -0,0 +1,131 @@
+<php
+
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/fiche_appart.css">
+    <link rel="stylesheet" href="../assets/css/font.css">
+    <title>OIKOS</title>
+</head>
+<body>
+<header>
+    <nav>
+        <a href="#" class="logo">OIKOS</a>
+        <div class="button_home">
+            <a href="">
+            <img src="../assets/images/menu.svg" width="20" height="20">
+            </a>
+            <a href="">
+            <img src="../assets/images/user.svg" width="20" height="20">
+            </a>
+        </div>
+    </nav>
+</header>
+<div class="acceuil">
+    <div class="nom_appart">
+        <p>LE MINUIT</p>
+    </div>
+</div>
+<div class="info_prix">
+    <div class="title_et_info">
+        <div class="title_appart">
+            <p>Opéra Garnier, 9e arrondissement, Paris</p>
+        </div>
+        <div class="info_appart">
+            <p>10 voyageurs - 5 chambres - 5 lits - 2 salles de bains</p>
+        </div>
+    </div>
+    <div class="equipements">
+        <div class="logo_and_title">
+            <div class="meeting_room">
+                <div class="meeting_room_logo">
+                    <img src="" alt="">
+                </div>
+                <div class="meeting_room_title">
+                    <p>Meeting<br>Room</p>
+                </div>
+            </div>
+            <div class="piano">
+                <div class="piano_logo">
+                    <img src="" alt="">
+                </div>
+                <div class="piano_title">
+                    <p>Piano</p>
+                </div>
+            </div>
+            <div class="home_cinema">
+                <div class="home_cinema_logo">
+                    <img src="" alt="">
+                </div>
+                <div class="home_cinema_title">
+                    <p>Home<br>Cinema</p>
+                </div>
+            </div>
+            <div class="bar">
+                <div class="bar_logo">
+                    <img src="" alt="">
+                </div>
+                <div class="bar_title">
+                    <p>Bar</p>
+                </div>
+            </div>
+            <div class="belle_vue">
+                <div class="belle_vue_logo">
+                    <img src="" alt="">
+                </div>
+                <div class="belle_vue_title">
+                    <p>Belle<br>Vue</p>
+                </div>
+            </div>
+            <div class="pmr">
+                <div class="pmr_logo">
+                    <img src="" alt="">
+                </div>
+                <div class="pmr_title">
+                    <p>PMR</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="prestations_incluses">
+        <div class="prestations_incluses_title">
+            <p>Prestations incluses</p>
+        </div>
+        <div class="prestations_incluses_text">
+            <p>Room tour</p>
+            <p>Bouteille de champagne</p>
+            <p>Chocolat de Paris</p>
+            <p>Apéritif de bienvenue</p>
+        </div>
+    </div>
+    <div class="le_logement">
+        <div class="le_logement_title">
+            <p>Le logement</p>
+        </div>
+        <div class="le_logement_text">
+            <p>Situé au cœur de Paris, cet appartement somptueux et élégant offre une vue imprenable sur l'Opéra Garnier, l'un des joyaux architecturaux de la Ville Lumière. Niché au sein d'un immeuble haussmannien, ce lieu empreint d'histoire et de charme combine harmonieusement le style classique français et les commodités modernes.</p>
+        </div>
+    </div>
+    <div class="tarif_card">
+        <div class="tarif_card_price_advices">
+            <div class="tarif_card_price">
+                <p>2 320 € par nuit</p>
+            </div>
+            <div class="tarif_card_advices">
+                <p>• 8 commentaires</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+</body>
+</html>
