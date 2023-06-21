055e494e0c1a4b0db690d3201d6bfb984ab1cb0d
victan78 <116087124+Victan78@users.noreply.github.com>
2023-06-21 11:47:07 +0200

    update message

--- client_zone/clients/get_message.php
+++ client_zone/clients/get_message.php
@@ -1,12 +1,14 @@
 <?php
-require_once '../../inc/pdo.php';
+require_once '../pdo.php';
 session_start();
-$client_id = $_SESSION['id'];
+$client_id = $_GET['client_id'];
+$booking_id = $_GET['booking_id'];
 //recuperer les messages de la base de donnees
-$requete = $website_pdo->prepare("SELECT * FROM booking_messaging where client_id = :client_id ");
+$requete = $website_pdo->prepare("SELECT * FROM booking_messaging where client_id = :client_id and booking_id = :booking_id");
 $requete->execute(
     array(
-        ':client_id' => $client_id
+        ':client_id' => $client_id,
+        ':booking_id' => $booking_id
     )
 );
 $messages = $requete->fetchAll(PDO::FETCH_ASSOC);
--- messagerie_oiokos/gestion/get_message.php
+++ messagerie_oiokos/gestion/get_message.php
@@ -1,11 +1,13 @@
 <?php
 require_once '../pdo.php';
 session_start();
+$booking_id = $_GET['booking_id'];
 $client_id = $_GET['client_id'];
 //recuperer les messages de la base de donnees
-$requete = $website_pdo->prepare("SELECT * FROM booking_messaging where client_id = :client_id");
+$requete = $website_pdo->prepare("SELECT * FROM booking_messaging where booking_id = :booking_id and client_id = :client_id");
 $requete->execute(
     array(
+        ':booking_id' => $booking_id,
         ':client_id' => $client_id
     )
 );
