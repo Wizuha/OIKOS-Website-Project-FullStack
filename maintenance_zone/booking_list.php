53b0ee2913c07deb9e6e6ea0ea22cf8deff4c68a
shay <shaynna.raymond@gmail.com>
2023-06-22 14:03:00 +0200

    adding the check function

--- maintenance_zone/booking_list.php
+++ maintenance_zone/booking_list.php
@@ -3,113 +3,107 @@ session_start();
 require '../inc/pdo.php';
 require '../inc/functions/token_function.php';
 
-// Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['id'])) {
-    echo "Vous devez être connecté pour accéder à cette page.";
-    exit;
+if(isset($_SESSION['token'])){
+    $check = token_check($_SESSION["token"], $website_pdo, $_SESSION['id']);
+    if($check == 'false'){
+        header('Location: ../connection/login.php');
+        exit();
+    }else {
+        if ($_SESSION['status'] == 0) {
+            header ('Location: ../inc/tpl/inactive_user.html');
+            exit(); 
+        }
+        if ($_SESSION['maintenance_role'] == 0){
+            header ('Location: ../public_zone/homepage.php');
+            exit();
+        }
+    }   
+}elseif(!isset($_SESSION['token'])){
+    header('Location: ../connection/login.php');
+    exit();
 }
 
-// Vérifier les autorisations d'accès (rôle d'entretien)
-$role_requete = $website_pdo->prepare('
-SELECT id, maintenance_role, management_role, admin_role  
-FROM user
-WHERE id = :id;
-');
 
-$role_requete->execute([
-    'id' => $_SESSION['id']
-]);
-$role_result = $role_requete->fetch(PDO::FETCH_ASSOC);
+// Récupérer le mois actuel
+$currentMonth = date('Y-m');
 
-if ($role_result && $role_result['maintenance_role'] == 1 or $role_result && $role_result['management_role'] == 1 or $role_result && $role_result['admin_role'] == 1) {
-    $maintenanceRole = $role_result['maintenance_role'];
-    // Si le rôle maintenance_role est ok :
+// Vérifier si un mois différent a été sélectionné
+if (isset($_GET['month'])) {
+    $selectedMonth = $_GET['month'];
+} else {
+    $selectedMonth = $currentMonth;
+}
 
-    // Récupérer le mois actuel
-    $currentMonth = date('Y-m');
+// Requête pour récupérer les dates de réservation de chaque logement
+$reservationQuery = "
+    SELECT housing.id AS housing_id, housing.title, booking.start_date_time, booking.end_date_time
+    FROM housing
+    LEFT JOIN booking ON housing.id = booking.housing_id
+    WHERE DATE_FORMAT(booking.start_date_time, '%Y-%m') = :selectedMonth
+    ORDER BY housing.id, booking.start_date_time
+";
+$reservationStmt = $website_pdo->prepare($reservationQuery);
+$reservationStmt->bindParam(':selectedMonth', $selectedMonth, PDO::PARAM_STR);
+$reservationStmt->execute();
+$reservations = $reservationStmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Vérifier si un mois différent a été sélectionné
-    if (isset($_GET['month'])) {
-        $selectedMonth = $_GET['month'];
-    } else {
-        $selectedMonth = $currentMonth;
-    }
+// Tableau pour stocker les dates de réservation de chaque logement
+$housingReservations = [];
 
-    // Requête pour récupérer les dates de réservation de chaque logement
-    $reservationQuery = "
-        SELECT housing.id AS housing_id, housing.title, booking.start_date_time, booking.end_date_time
-        FROM housing
-        LEFT JOIN booking ON housing.id = booking.housing_id
-        WHERE DATE_FORMAT(booking.start_date_time, '%Y-%m') = :selectedMonth
-        ORDER BY housing.id, booking.start_date_time
-    ";
-    $reservationStmt = $website_pdo->prepare($reservationQuery);
-    $reservationStmt->bindParam(':selectedMonth', $selectedMonth, PDO::PARAM_STR);
-    $reservationStmt->execute();
-    $reservations = $reservationStmt->fetchAll(PDO::FETCH_ASSOC);
+// Organiser les dates de réservation par logement
+foreach ($reservations as $reservation) {
+    $housingId = $reservation['housing_id'];
 
-    // Tableau pour stocker les dates de réservation de chaque logement
-    $housingReservations = [];
-
-    // Organiser les dates de réservation par logement
-    foreach ($reservations as $reservation) {
-        $housingId = $reservation['housing_id'];
-
-        if (!isset($housingReservations[$housingId])) {
-            $housingReservations[$housingId] = [
-                'title' => $reservation['title'],
-                'dates' => []
-            ];
-        }
-
-        $housingReservations[$housingId]['dates'][] = [
-            'start_date' => $reservation['start_date_time'],
-            'end_date' => $reservation['end_date_time']
+    if (!isset($housingReservations[$housingId])) {
+        $housingReservations[$housingId] = [
+            'title' => $reservation['title'],
+            'dates' => []
         ];
     }
 
-    // Affichage du tableau des réservations par logement
-    echo "<h2>Booking à venir</h2>";
+    $housingReservations[$housingId]['dates'][] = [
+        'start_date' => $reservation['start_date_time'],
+        'end_date' => $reservation['end_date_time']
+    ];
+}
 
-    // Affichage des flèches pour passer d'un mois à un autre
-    echo $selectedMonth;
-    echo '<a href="?month=' . date('Y-m', strtotime($selectedMonth . ' -1 month')) . '">&lt; Mois précédent</a> | ';
-    echo '<a href="?month=' . date('Y-m', strtotime($selectedMonth . ' +1 month')) . '">Mois suivant &gt;</a>';
+// Affichage du tableau des réservations par logement
+echo "<h2>Booking à venir</h2>";
 
-    if (count($housingReservations) > 0) {
-        echo "<table>";
+// Affichage des flèches pour passer d'un mois à un autre
+echo $selectedMonth;
+echo '<a href="?month=' . date('Y-m', strtotime($selectedMonth . ' -1 month')) . '">&lt; Mois précédent</a> | ';
+echo '<a href="?month=' . date('Y-m', strtotime($selectedMonth . ' +1 month')) . '">Mois suivant &gt;</a>';
+
+if (count($housingReservations) > 0) {
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>ID du logement</th>";
+    echo "<th>Logement</th>";
+    echo "<th>Dates de réservation</th>";
+    echo "</tr>";
+
+    foreach ($housingReservations as $housingId => $housingReservation) {
         echo "<tr>";
-        echo "<th>ID du logement</th>";
-        echo "<th>Logement</th>";
-        echo "<th>Dates de réservation</th>";
-        echo "</tr>";
+        echo "<td>" . $housingId . "</td>";
+        echo "<td>" . htmlspecialchars($housingReservation['title']) . "</td>";
+        echo "<td>";
 
-        foreach ($housingReservations as $housingId => $housingReservation) {
-            echo "<tr>";
-            echo "<td>" . $housingId . "</td>";
-            echo "<td>" . htmlspecialchars($housingReservation['title']) . "</td>";
-            echo "<td>";
-
-            foreach ($housingReservation['dates'] as $reservationDate) {
-                $startDate = date_format(date_create($reservationDate['start_date']), 'd/m/Y');
-                $endDate = date_format(date_create($reservationDate['end_date']), 'd/m/Y');
-                echo "Début du séjour : " . $startDate . " - " . "Fin du séjour: " . $endDate . "<br>";
-            }
-
-            echo "</td>";
-            echo "</tr>";
+        foreach ($housingReservation['dates'] as $reservationDate) {
+            $startDate = date_format(date_create($reservationDate['start_date']), 'd/m/Y');
+            $endDate = date_format(date_create($reservationDate['end_date']), 'd/m/Y');
+            echo "Début du séjour : " . $startDate . " - " . "Fin du séjour: " . $endDate . "<br>";
         }
 
-        echo "</table>";
-    } else {
-        echo "Pas de réservation pour ce mois.";
+        echo "</td>";
+        echo "</tr>";
     }
 
-// L'utilisateur n'a pas le rôle nécessaire -> le rediriger vers l'accueil ou quelque chose comme ça :
-}else {
-    echo "Vous n'avez pas les droits pour continuer.";
-    exit;
+    echo "</table>";
+} else {
+    echo "Pas de réservation pour ce mois.";
 }
+
 ?>
 
 <!DOCTYPE html>
--- maintenance_zone/maintenance_details.php
+++ maintenance_zone/maintenance_details.php
@@ -3,101 +3,95 @@ session_start();
 require '../inc/pdo.php';
 require '../inc/functions/token_function.php';
 
-// Vérifier si l'utilisateur est connecté
-if (!isset($_SESSION['id'])) {
-    echo "Vous devez être connecté pour accéder à cette page.";
-    exit;
+if(isset($_SESSION['token'])){
+    $check = token_check($_SESSION["token"], $website_pdo, $_SESSION['id']);
+    if($check == 'false'){
+        header('Location: ../connection/login.php');
+        exit();
+    }else {
+        if ($_SESSION['status'] == 0) {
+            header ('Location: ../inc/tpl/inactive_user.html');
+            exit(); 
+        }
+        if ($_SESSION['maintenance_role'] == 0){
+            header ('Location: ../public_zone/homepage.php');
+            exit();
+        }
+    }   
+}elseif(!isset($_SESSION['token'])){
+    header('Location: ../connection/login.php');
+    exit();
 }
 
-// Vérifier les autorisations d'accès (rôle d'entretien)
-$role_requete = $website_pdo->prepare('
-    SELECT id, maintenance_role
-    FROM user
-    WHERE id = :id;
-');
+// Vérifier si l'ID de la maintenance est spécifié dans l'URL
+if (isset($_GET['id'])) {
+    $maintenanceId = $_GET['id'];
 
-$role_requete->execute([
-    'id' => $_SESSION['id']
-]);
-$role_result = $role_requete->fetch(PDO::FETCH_ASSOC);
+    // Récupérer les détails de la maintenance à partir de l'ID
+    $maintenance_requete = $website_pdo->prepare('
+        SELECT m.id, m.status, m.title, m.schedule_date, m.housing_id, hi.image, h.title AS housing_title
+        FROM maintenance m
+        JOIN housing_image hi ON m.housing_id = hi.housing_id  
+        JOIN housing h ON m.housing_id = h.id
+        WHERE m.id = :maintenanceId
+    ');
+    $maintenance_requete->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
+    $maintenance_requete->execute();
+    $maintenance_details = $maintenance_requete->fetch(PDO::FETCH_ASSOC);
 
-if ($role_result && $role_result['maintenance_role'] == 1) {
-    $maintenanceRole = $role_result['maintenance_role'];
-
-    // Vérifier si l'ID de la maintenance est spécifié dans l'URL
-    if (isset($_GET['id'])) {
-        $maintenanceId = $_GET['id'];
-
-        // Récupérer les détails de la maintenance à partir de l'ID
-        $maintenance_requete = $website_pdo->prepare('
-            SELECT m.id, m.status, m.title, m.schedule_date, m.housing_id, hi.image, h.title AS housing_title
-            FROM maintenance m
-            JOIN housing_image hi ON m.housing_id = hi.housing_id  
-            JOIN housing h ON m.housing_id = h.id
-            WHERE m.id = :maintenanceId
-        ');
-        $maintenance_requete->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
-        $maintenance_requete->execute();
-        $maintenance_details = $maintenance_requete->fetch(PDO::FETCH_ASSOC);
-
-    } else {
-        echo "ID de maintenance non spécifié.";
-    }
+} else {
+    echo "ID de maintenance non spécifié.";
+}
 
 
-    $title = "Checklist: ";
+$title = "Checklist: ";
 
-    // Traitement du formulaire après soumission
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $maintenanceNote = $_POST['maintenance_note'];
+// Traitement du formulaire après soumission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $maintenanceNote = $_POST['maintenance_note'];
 
-        // Vérifier si toutes les cases sont cochées
-        $isChecked = true;
-        if (isset($maintenance_check) && is_array($maintenance_check)) {
-            foreach ($maintenance_check as $maintenance) {
-                if (empty($check)) {
-                    $isChecked = false;
-                    break;
-                }
-                
+    // Vérifier si toutes les cases sont cochées
+    $isChecked = true;
+    if (isset($maintenance_check) && is_array($maintenance_check)) {
+        foreach ($maintenance_check as $maintenance) {
+            if (empty($check)) {
+                $isChecked = false;
+                break;
             }
-            }
-
-        // Mettre à jour le statut en fonction de la validation
-        if ($isChecked) {
-            $scheduleDate = '';
-            $status = 'fait';
-            // Supprimer la maintenance si le mois actuel se termine
-            $currentMonthEnd = date('Y-m-t');
-            if ($scheduleDate === $currentMonthEnd) {
-                // Supprimer la maintenance
-                $deleteMaintenance = $website_pdo->prepare('DELETE FROM maintenance WHERE id = :maintenanceId');
-                $deleteMaintenance->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
-                $deleteMaintenance->execute();
-            }
-        } else {
-            $status = 'en cours';
+            
+        }
         }
 
-        // Mettre à jour le statut dans la table de maintenance
-        $updateMaintenance = $website_pdo->prepare('UPDATE maintenance SET status = :status WHERE id = :maintenanceId');
-        $updateMaintenance->bindParam(':status', $status, PDO::PARAM_STR);
-        $updateMaintenance->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
-        $updateMaintenance->execute();
-
-        // Insérer la note de maintenance
-        $insertNote = $website_pdo->prepare('INSERT INTO maintenance_note (maintenance_id, user_id, content) VALUES (:maintenanceId, :userId, :content)');
-        $insertNote->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
-        $insertNote->bindParam(':userId', $_SESSION['id'], PDO::PARAM_INT);
-        $insertNote->bindParam(':content', $maintenanceNote, PDO::PARAM_STR);
-        $insertNote->execute();
+    // Mettre à jour le statut en fonction de la validation
+    if ($isChecked) {
+        $scheduleDate = '';
+        $status = 'fait';
+        // Supprimer la maintenance si le mois actuel se termine
+        $currentMonthEnd = date('Y-m-t');
+        if ($scheduleDate === $currentMonthEnd) {
+            // Supprimer la maintenance
+            $deleteMaintenance = $website_pdo->prepare('DELETE FROM maintenance WHERE id = :maintenanceId');
+            $deleteMaintenance->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
+            $deleteMaintenance->execute();
+        }
+    } else {
+        $status = 'en cours';
     }
 
-// L'utilisateur n'a pas le rôle nécessaire -> le rediriger vers l'accueil ou quelque chose comme ça :
-} else {
-    echo "Vous n'avez pas les autorisations nécessaires pour accéder à cette page.";
-    exit;
+    // Mettre à jour le statut dans la table de maintenance
+    $updateMaintenance = $website_pdo->prepare('UPDATE maintenance SET status = :status WHERE id = :maintenanceId');
+    $updateMaintenance->bindParam(':status', $status, PDO::PARAM_STR);
+    $updateMaintenance->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
+    $updateMaintenance->execute();
+
+    // Insérer la note de maintenance
+    $insertNote = $website_pdo->prepare('INSERT INTO maintenance_note (maintenance_id, user_id, content) VALUES (:maintenanceId, :userId, :content)');
+    $insertNote->bindParam(':maintenanceId', $maintenanceId, PDO::PARAM_INT);
+    $insertNote->bindParam(':userId', $_SESSION['id'], PDO::PARAM_INT);
+    $insertNote->bindParam(':content', $maintenanceNote, PDO::PARAM_STR);
+    $insertNote->execute();
 }
+
 ?>
 
 <!DOCTYPE html>
